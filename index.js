ecdd7e57e206cb50bc3f83780490af4dafd1de76
Ramadhan <ramiqcom@gmail.com>
2023-09-27 08:27:06 +0700

    add table viewer

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 // Map data
-const Map = L.map('map').setView([0, 117], 5);
+const map = L.map('map').setView([0, 117], 5);
 
 // Tile 
 L.tileLayer('http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}', {
 	attribution: 'Google Maps'
-}).addTo(Map);
+}).addTo(map);
 
 // Data tile
-const layers = document.getElementById('panel');
+const layers = document.getElementById('layers');
+
+// Dialog table
+const tabPanel = document.getElementById('table');
+tabPanel.style.width = '50%';
+const buttonDiv = document.createElement('div');
+buttonDiv.style.display = 'flex';
+buttonDiv.style.flexDirection = 'row-reverse';
+const xbutton = document.createElement('button');
+xbutton.onclick = () => tabPanel.close();
+xbutton.append('X');
+tabPanel.prepend(xbutton);
+const gridDiv = document.getElementById('tablegrid');
+const grid = new gridjs.Grid({
+	data: [['No data']],
+	columns: ['No data'],
+	resizable: true,
+	autoWidth: true,
+	fixedHeader: true,
+	search: true,
+	sort: true,
+	height: '40vh',
+	style: {
+		container: {
+			fontSize: 'small'
+		}
+	}
+});
+grid.render(gridDiv);	
 
 // Dialog div
 const dialogDiv = document.getElementById('errorMsg');
@@ -106,16 +134,16 @@ async function vectorLayer(format, file){
 	};
 
 	// Vector tle
-	const vectorTile = L.geoJson.vt(geojson, optionsVector).addTo(Map);
+	const vectorTile = L.geoJson.vt(geojson, optionsVector).addTo(map);
 
 	// Zoom to bounds
-	Map.fitBounds(bounds);
+	map.fitBounds(bounds);
 
 	// Set data
 	data = vectorTile;
 
 	// Add layer control
-	addLayers(vectorTile, file.name);
+	addLayers(vectorTile, file.name, bounds, 'vector', geojson);
 }
 
 // Function to load raster data
@@ -125,17 +153,17 @@ async function rasterLayer(file){
 		georaster: imageTile,
 		opacity: 1,
 		resolution: 1024
-	}).addTo(Map);
+	}).addTo(map);
 	
 	const bounds = imageTile.extent.leafletBounds;
 
-	Map.fitBounds(bounds);
+	map.fitBounds(bounds);
 
 	// Set data
 	data = imageTile;
 
 	// Add layer control
-	addLayers(imageTile, file.name);
+	addLayers(imageTile, file.name, bounds, 'raster', null);
 }
 
 // Error show
@@ -146,34 +174,94 @@ function errorShow(msg){
 }
 
 // Function to create a div
-function addLayers(layer, name){
+function addLayers(layer, name, bounds, type, geojson){
+	// Main element
 	const div = document.createElement('div');
 	div.style.display = 'flex';
 	div.style.gap = '1%';
-	div.style.justifyContent = 'flex-between';
+	div.style.justifyContent = 'space-between';
+	div.style.alignContent = 'center';
+	div.style.border = '0.5px solid black';
+	div.style.padding = '1%';
+	layers.prepend(div);
 	
+	// First div
+	const first = document.createElement('div');
+	first.style.display = 'flex';
+	first.style.gap = '5px';
+	div.append(first);
+
+	// Checkbox
 	const check = document.createElement('input');
 	check.setAttribute('type', 'checkbox');
 	check.setAttribute('checked', true);
 	check.onchange = e => {
 		const status = e.target.checked;
-		status ? layer.addTo(Map) : Map.removeLayer(layer);
+		if (type == 'vector'){
+			status ? layer.options.opacity = 1: layer.options.opacity = 0;
+			layer.redraw();
+		}
+
+		if (type == 'raster'){
+			status ? layer.setOpacity(1): layer.setOpacity(0);
+		}
 	};
-	div.append(check);
+	first.append(check);
 	
 	// Name
 	const label = document.createElement('div')
 	label.append(name);
-	div.append(label);
+	first.append(label);
 
-	// Button to remove layer definetrly
-	const button = document.createElement('button');
-	button.onclick = () => {
-		Map.removeLayer(layer);
+	// Second div
+	const second = document.createElement('div');
+	second.style.display = 'flex';
+	second.style.gap = '5px';
+	div.append(second);
+
+	if (geojson) {
+		// Grid
+		let columns = Object.keys(geojson.features[0].properties).map(key => { return { id: key, name: key} });
+		let data = geojson.features.map(feat => feat.properties);
+		
+		// Set table data on gridjs
+		if (!(columns.length && data.length)) {
+			columns = ['No data'];
+			data = [['No data']];
+		};
+
+		// Updata data based column and data
+		grid.updateConfig({
+			data,
+			columns
+		}).forceRender();
+
+		// Table button
+		const table = document.createElement('button');
+		// Render table on click table button
+		table.onclick = () => {
+			tabPanel.showModal();
+		};
+		table.append('Table');
+
+		// Add table button to layers
+		second.append(table);	
+	}
+
+	// Zoom to layer
+	const zoom = document.createElement('button');
+	zoom.onclick = () => {
+		map.fitBounds(bounds);
+	};
+	zoom.append('Pan');
+	second.append(zoom);
+
+	// Button to remove layer definetly
+	const remove = document.createElement('button');
+	remove.onclick = () => {
+		map.removeLayer(layer);
 		div.remove();
 	};
-	button.append('Remove');
-	div.append(button);
-
-	layers.append(div);
+	remove.append('X');
+	second.append(remove);
 }
